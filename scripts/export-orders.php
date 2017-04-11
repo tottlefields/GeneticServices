0e66c5066187d5cec9d8c876a2d348572665bbed
Ellen Schofield <tottlefields@gmail.com>
2017-04-11 17:14:42 +0100

    start of script for exporting orders regularly for cron job

---
+++ scripts/export-orders.php
@@ -0,0 +1,21 @@
+<?php
+global $wpdb;
+
+//$NOW = strtotime('2017-04-10 12:05:00');
+$NOW = time();
+
+$_REQUEST['from'] = date('Y-m-d H:i:s', $NOW);
+$_REQUEST['start']  = date('Y-m-d H:i:s', strtotime('-1 hour', strtotime($_REQUEST['from'])));
+
+$data = getOrderDataForCsv();
+$header = array_shift($data);
+
+if (count($data) > 0){
+	$file = outputCSV($data);
+	echo $file."\n";
+}
+else{
+	echo $_REQUEST['from'].": No orders to export\n";
+}
+
+?>
