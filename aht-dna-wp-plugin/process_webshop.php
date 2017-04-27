9ec6ccc56fa0dc76fc422e0c3b251440ce881c0a
eschofield <eschofield@CPM207.aht.org.uk>
2017-04-27 16:50:07 +0100

    updates for loading new clients/animals in

--- aht-dna-wp-plugin/process_webshop.php
+++ aht-dna-wp-plugin/process_webshop.php
@@ -21,7 +21,7 @@ if ($DEBUG) { echo str_replace("\t", "", $SQL)."\n"; }
 $RESULT = mysqli_query($mysqli, $SQL);
 while ($row = mysqli_fetch_assoc($RESULT)){
 	
-	$SQL2 = 'SELECT id, ClientID FROM client WHERE 
+	$SQL2 = 'SELECT * FROM client WHERE 
 			(FullName = "'.$row['FullName'].'") + 
 			(Email = "'.$row['Email'].'") + 
 			(ClientID = '.$row['client_id'].') > 2';
@@ -32,19 +32,18 @@ while ($row = mysqli_fetch_assoc($RESULT)){
 	}
 	else{
 		echo "Client needs a record creating in the database\n";
-		$SQL3 = 'INSERT INTO client(FullName, Organisation, Email, Tel, Fax, Address, Address2, Address3, Town, County, Postcode,
+		$SQL3 = 'INSERT INTO client(ClientID, FullName, Organisation, Email, Tel, Fax, Address, Address2, Address3, Town, County, Postcode,
 				Country, ShippingName, ShippingCompany, ShippingAddress, ShippingAddress2, ShippingAddress3, ShippingTown, ShippingCounty, ShippingPostcode, ShippingCountry)
-				VALUES ("'.$row['client_id'].'", "'.$SPECIES[$row['Species']].'", "'.$row['Breed'].'", "'.$row['RegisteredName'].'", "'.$row['Registration'].'", "'.$row['Sex'].'", "'.$row['TatooOrChip'].'", "'.$row['BirthDate'].'", "'.$row['PetName'].'", "'.$row['Colour'].'")';
+				VALUES ("'.$row['client_id'].'", "'.$row['FullName'].'", "'.$row['Organisation'].'", "'.$row['Email'].'", "'.$row['Tel'].'", "'.$row['Fax'].'", "'.$row['Address'].'", "'.$row['Address2'].'", "'.$row['Address3'].'", "'.$row['Town'].'",
+						"'.$row['County'].'", "'.$row['Postcode'].'", "'.$row['Country'].'", "'.$row['ShippingName'].'", "'.$row['ShippingCompany'].'", "'.$row['ShippingAddress'].'", "'.$row['ShippingAddress2'].'", "'.$row['ShippingTown'].'", 
+						"'.$row['ShippingCounty'].'", "'.$row['ShippingPostcode'].'", "'.$row['ShippingCountry'].'")';
 		if ($DEBUG) { echo str_replace("\t", "", $SQL3)."\n"; }
 	}
 }
 
 
-
-exit(0);
-
 //ANIMALS
-$SQL = "SELECT GROUP_CONCAT(AnimalID) as animal_ids, Species, Breed, RegisteredName, Registration, Sex, date_BirthDate, TatooOrChip, Colour, PetName 
+$SQL = "SELECT GROUP_CONCAT(AnimalID) as animal_ids, min(AnimalID) as AnimalID, ClientID, Species, Breed, RegisteredName, Registration, Sex, date_BirthDate, TatooOrChip, Colour, PetName 
 		FROM webshop_import WHERE imported=0 GROUP BY RegisteredName, Registration, PetName, date_BirthDate, TatooOrChip";
 if ($DEBUG) { echo str_replace("\t", "", $SQL)."\n"; }
 
@@ -52,7 +51,7 @@ $RESULT = mysqli_query($mysqli, $SQL);
 while ($row = mysqli_fetch_assoc($RESULT)){
 	//echo implode("\t", array($row['RegisteredName'], $row['Registration'], $row['date_BirthDate'], $row['TatooOrChip']))."\n";
 	
-	$SQL2 = 'SELECT id, AnimalID FROM animal WHERE 
+	$SQL2 = 'SELECT * FROM animal WHERE 
 			(RegisteredName = "'.$row['RegisteredName'].'") + 
 			(PetName = "'.$row['PetName'].'") + 
 			(Registration = "'.$row['Registration'].'") + 
@@ -66,7 +65,8 @@ while ($row = mysqli_fetch_assoc($RESULT)){
 	else{
 		echo "Animal needs a record creating in the database\n";
 		$SQL3 = 'INSERT INTO animal(webshop_animal_ids, Species, Breed, RegisteredName, Registration, Sex, TatooOrChip, BirthDate, PetName, Colour)
-				VALUES ("'.$row['animal_ids'].'", "'.$SPECIES[$row['Species']].'", "'.$row['Breed'].'", "'.$row['RegisteredName'].'", "'.$row['Registration'].'", "'.$row['Sex'].'", "'.$row['TatooOrChip'].'", "'.$row['BirthDate'].'", "'.$row['PetName'].'", "'.$row['Colour'].'")';
+				VALUES ("'.$row['animal_ids'].'", '.$row['AnimalID'].', '.$row['ClientID'].', "'.$SPECIES[$row['Species']].'", "'.$row['Breed'].'", "'.$row['RegisteredName'].'", "'.$row['Registration'].'", "'.$row['Sex'].'", 
+						"'.$row['TatooOrChip'].'", "'.$row['BirthDate'].'", "'.$row['PetName'].'", "'.$row['Colour'].'")';
 		if ($DEBUG) { echo str_replace("\t", "", $SQL3)."\n"; }
 	}
 }
