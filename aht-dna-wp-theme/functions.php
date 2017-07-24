fafa2c945c014551a48d7c30a8c8b94462cee2be
eschofield <eschofield@CPM207.aht.org.uk>
2017-07-24 11:34:48 +0100

    altered wordpress email address and sender name

--- aht-dna-wp-theme/functions.php
+++ aht-dna-wp-theme/functions.php
@@ -9,6 +9,22 @@ register_nav_menu(
 	'Main Menu'
 );
 
+// Function to change email address
+
+function wpb_sender_email( $original_email_address ) {
+	return 'dennis@aht.org.uk';
+}
+
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+	return 'DENNIS';
+}
+
+// Hooking up our functions to WordPress filters
+add_filter( 'wp_mail_from', 'wpb_sender_email' );
+add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
+
+
 
 
 function mytheme_enqueue_scripts() {
