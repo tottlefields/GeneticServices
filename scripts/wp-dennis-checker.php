c7c8002b2a140788ae3fee2569826f4e27fd7ec7
eschofield <eschofield@CPM207.aht.org.uk>
2017-10-10 10:25:43 +0100

    added checks for animal and client records having errors when loaded

--- scripts/wp-dennis-checker.php
+++ scripts/wp-dennis-checker.php
@@ -106,6 +106,9 @@ foreach( $posts as $post ) {
 		);
 		$clients[$clientId] = $client;
 		$wpdb->replace('client', $client);
+		if ($wpdb->last_error) {
+			echo 'ERROR detected when inserting client with ClientID=' . $clientId . "\n" . $wpdb->last_error;
+		}
 	}
 	
 	$animalId = $postMeta['animal-id-pm'][0];
@@ -128,6 +131,9 @@ foreach( $posts as $post ) {
 		);
 		$animals[$animalId] = $animal;
 		$wpdb->replace('animal', $animal);
+		if ($wpdb->last_error) {
+			echo 'ERROR detected when inserting client with ClientID=' . $clientId . "\n" . $wpdb->last_error;
+		}
 	}
 	
 	$MAX_ID = $post->ID;
