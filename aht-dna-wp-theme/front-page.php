88c72ab5d42ce8b1d289e245c8d9035ab9dbb2b2
tottlefields <ellen.schofield@aht.org.uk>
2017-10-25 14:37:45 +0100

    frontpage alterations

--- aht-dna-wp-theme/front-page.php
+++ aht-dna-wp-theme/front-page.php
@@ -26,30 +26,30 @@
 
 					<section class="row text-center placeholders">
 						<div class="col-md-3 col-sm-6 placeholder">
-							<h4>This Month</h4>
-							<div class="circle" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/circle-purple.png'; ?>);">
-								<div style="line-height:200px;"><i class="fa fa-flask" aria-hidden="true"></i><?php echo countTests(date('Y'), date('m')); ?></div>
+							<div class="statbox purple">
+								<div class="number"><?php echo countOrders($order_steps[0]); ?></div>
+								<div class="title">Pending Orders</div>
+								<div class="footer"><a href="/orders/"> View list</a></div>
 							</div>
-							<div class="text-muted">Tests ordered this month.</div>
 						</div>
 						<div class="col-md-3 col-sm-6 placeholder">
-							<h4>This Year</h4>
-							<div class="circle" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/circle-green.png'; ?>);">
-								<div style="line-height:200px;"><i class="fa fa-flask" aria-hidden="true"></i><?php echo countTests(date('Y')); ?></div>						
+							<div class="statbox yellow">
+								<div class="number"><?php echo countOrders($order_steps[1]); ?></div>
+								<div class="title">Untested Samples</div>
+								<div class="footer"><a href="/orders/"> View list</a></div>
 							</div>
-							<div class="text-muted">Tests ordered this year.</div>
 						</div>
 						<div class="col-md-3 col-sm-6 placeholder">
 							<h4>This Month</h4>
-							<div class="circle" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/circle-purple.png'; ?>);">
-								<div style="line-height:200px;"><i class="fa fa-shopping-basket" aria-hidden="true"></i><?php echo countOrders(date('Y'), date('m')); ?></div>	
+							<div class="statbox blue">
+								<div style="line-height:200px;"><i class="fa fa-shopping-basket" aria-hidden="true"></i><?php echo orderStats(date('Y'), date('m')); ?></div>	
 							</div>
 							<div class="text-muted">Total orders this month.</div>
 						</div>
 						<div class="col-md-3 col-sm-6 placeholder">
 							<h4>This Year</h4>
-							<div class="circle" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/img/circle-green.png'; ?>);">
-								<div style="line-height:200px;"><i class="fa fa-shopping-basket" aria-hidden="true"></i><?php echo countOrders(date('Y')); ?></div>	
+							<div class="statbox green">
+								<div style="line-height:200px;"><i class="fa fa-shopping-basket" aria-hidden="true"></i><?php echo orderStats(date('Y')); ?></div>	
 							</div>
 							<div class="text-muted">Total orders this year.</div>
 						</div>
