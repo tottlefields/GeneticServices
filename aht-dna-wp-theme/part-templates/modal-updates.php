f0cec0194ec9474b825f48723153bef2d1eac8bb
tottlefields <ellen.schofield@aht.org.uk>
2017-10-31 12:34:37 +0000

    updates for shipping details on clients - modal edits

--- aht-dna-wp-theme/part-templates/modal-updates.php
+++ aht-dna-wp-theme/part-templates/modal-updates.php
@@ -1,6 +1,6 @@
 <?php
 
-$referer = (wp_get_referer()) ? wp_get_referer : $_SERVER['HTTP_REFERER'];
+$referer = (wp_get_referer()) ? wp_get_referer() : $_SERVER['HTTP_REFERER'];
  
 if(empty($_REQUEST)){
 	wp_safe_redirect($referer);
@@ -9,7 +9,7 @@ if(empty($_REQUEST)){
 
 global $wpdb;
 
-if (isset($_REQUEST['client-submitted'])){
+/*if (isset($_REQUEST['client-submitted'])){
 	
 	$data = array();
 	$where = array();
@@ -34,9 +34,55 @@ if (isset($_REQUEST['client-submitted'])){
 		$client_id = $wpdb->insert_id;
 		$wpdb->update('orders', array('client_id' => $client_id), array('id' => $_REQUEST['id']));
 	}
+	debug_array($data);
+	debug_array($where);
 	wp_safe_redirect($referer);
 	//wp_safe_redirect(get_site_url().'/orders/view/?id='.$_REQUEST['id']);
 	exit;	
+}*/
+
+if (isset($_REQUEST['client-submitted'])){
+	
+	unset($_REQUEST['id']);
+	unset($_REQUEST['client-submitted']);
+	
+	$order_data = array();
+	$order_where = array();
+	$client_data = array();
+	$client_where = array();
+	
+	foreach ($_REQUEST as $key => $value){
+		if (preg_match('/^client_/', $key)){
+			$new_key = str_replace('client_', '', $key);
+			if ($new_key === 'id'){ $client_where['id'] = $value;}
+			elseif (preg_match('/^Address/', $new_key)){
+				$address = array_pad(explode(', ', $value), 3, '');
+				$client_data[$new_key."3"] = array_pop($address);
+				$client_data[$new_key."2"] = array_pop($address);
+				$client_data[$new_key] = implode(', ', $address);
+			}
+			elseif (preg_match('/^Shipping/', $new_key)){
+				$order_data[$new_key] = $value;
+			}
+			else{
+				$client_data[$new_key] = $value;
+			}
+		}
+		if (preg_match('/^order_/', $key)){
+			$new_key = str_replace('order_', '', $key);
+			if ($new_key === 'id'){ $order_where['id'] = $value;}
+		}
+	}
+	if ($_REQUEST['client_id'] > 0){ $wpdb->update('client', $client_data, $client_where); }
+	elseif (count($client_data) > 0){
+		$wpdb->insert('client', $client_data);
+		$client_id = $wpdb->insert_id;
+		$wpdb->update('orders', array('client_id' => $client_id), array('id' => $_REQUEST['order_id']));
+	}
+	if ($_REQUEST['order_id'] > 0){ $wpdb->update('orders', $order_data, $order_where); }
+	
+	wp_safe_redirect($referer);
+	exit;
 }
 
 if (isset($_REQUEST['animal-submitted'])){
