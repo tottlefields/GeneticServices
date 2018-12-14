e9a37a5900530070763911a5f529431f4f9b4115
tottlefields <ellen.schofield@aht.org.uk>
2018-12-14 11:30:27 +0000

    work on adding plate records; sample page updates to better list untested samples

--- aht-dna-wp-theme/assets/js/plates.js
+++ aht-dna-wp-theme/assets/js/plates.js
@@ -1,7 +1,16 @@
 jQuery(document).ready(function($) {
+		
+		$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+				clearPlate();
+				console.log(e.target);				
+				console.log ( $(e.target).attr('id') );
+		})
+		
+		
 		$('.plate_select').on('change', function() {
 				
 				$("#plate_id").html($(this).val());
+				clearPlate();
 				
 				var data = {
 					'action' : 'plate_details',
@@ -25,4 +34,38 @@ jQuery(document).ready(function($) {
 						}
 				});
 		});
+		
+		$('#plate_type').change(function(e){
+				var plateType = $(this).val();
+				var plateIDs = plateJSON[plateType];
+				if (plateIDs.length > 0){
+					var last = plateIDs.pop();
+					console.log(last);
+					var plateID = "";
+					if (plateType == 'Extraction'){	
+						var id = parseInt(last.replace("Q", ""));
+						plateID = "Q"+(id+1);
+					}
+					else if (plateType == 'TaqMan'){	
+						var id = parseInt(last.replace("TaqMan", ""));
+						plateID = "TaqMan"+(id+1);
+					}
+					else if (plateType == 'Genotype'){	
+						var id = parseInt(last.replace("G", ""));
+						plateID = (id+1)+"G";
+					}
+					$('#new_plate').val(plateID);
+				}
+		});
 });
+
+function clearPlate(){
+	$("small.contents").html("");
+	$("small.cell_id").show();
+}
+	
+
+var postPlateForm = function() {
+	console.log(get_code);
+	return false;
+}
