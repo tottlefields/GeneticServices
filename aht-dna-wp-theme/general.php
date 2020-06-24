2bc6d9c76ea980f41b8c4578fb039ea95ee2fb89
Ellen Schofield <ellen.schofield@aht.org.uk>
2020-06-24 11:26:44 +0100

    added PortalID to Q plates

--- aht-dna-wp-theme/general.php
+++ aht-dna-wp-theme/general.php
@@ -299,9 +299,9 @@ function getPlateDetails($plate_id){
 	}
 	
 	if ($plate_details[0]->plate_type == 'extraction'){
-		$sql = "select distinct s.id as swab_id, t.order_id, s.test_id as test_id, t.test_code, s.swab, s.extraction_well as well 
+		$sql = "select distinct s.id as swab_id, t.order_id, s.test_id as test_id, PortalID, t.DDT_ID, t.test_code, s.swab, s.extraction_well as well 
 		from test_swabs s inner join order_tests t on s.test_id=t.id where s.extraction_plate='".$plate_id."'
-		order by 4,5,2,3";		
+		order by t.test_code, s.swab, t.order_id, s.test_id";		
 	}
 	else {
 		$sql = "select well_id as well, well_contents, test_code from plate_wells where test_plate='".$plate_id."'";
